c1a7df8c3eb6ea6c3681960e137d84a16b02197c
Aswanthcreator <aswanthshaji2009@gmail.com>
2025-11-28 20:25:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import os, math, logging
 import logging.config
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-#from Telethroid import started_telethroid
 from database.ia_filterdb import Media
 from database.users_chats_db import db
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_CHANNEL, PORT
@@ -11,9 +10,10 @@ from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 from datetime import datetime
 from pytz import timezone
-from pyrogram.errors import BadRequest, Unauthorized
+from pyrogram.errors import BadRequest, Unauthorized, FloodWait
 from plugins.__init__ import web_server
 from aiohttp import web
+import asyncio
 
 # Get logging configurations
 logging.config.fileConfig("logging.conf")
@@ -37,10 +37,18 @@ class Bot(Client):
         )
 
     async def start(self):
-        b_users, b_chats = await db.get_banned()
-        temp.BANNED_USERS = b_users
-        temp.BANNED_CHATS = b_chats        
-        await super().start()
+        try:
+            b_users, b_chats = await db.get_banned()
+            temp.BANNED_USERS = b_users
+            temp.BANNED_CHATS = b_chats        
+
+            await super().start()
+
+        except FloodWait as e:
+            logging.warning(f"FloodWait detected! Sleeping for {e.value} seconds...")
+            await asyncio.sleep(e.value)
+            return await self.start()
+
         await Media.ensure_indexes()
         me = await self.get_me()
         temp.ME = me.id
@@ -48,46 +56,60 @@ class Bot(Client):
         temp.B_NAME = me.first_name
         temp.B_LINK = me.mention
         self.username = '@' + me.username
+
         curr = datetime.now(timezone(TIMEZONE))
         date = curr.strftime('%d %B, %Y')
         time = curr.strftime('%I:%M:%S %p')
+
+        # Web server
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
         await web.TCPSite(app, bind_address, PORT).start()
-        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
-        #started_telethroid() # installation Telethroid Library   
+
+        logging.info(f"{me.first_name} started Pyrogram v{__version__} (Layer {layer}) on @{me.username}")
+
         if LOG_CHANNEL:
             try:
-                await self.send_message(LOG_CHANNEL, text=f"<b>{me.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!\n\n📅 Dᴀᴛᴇ : <code>{date}</code>\n⏰ Tɪᴍᴇ : <code>{time}</code>\n🌐 Tɪᴍᴇᴢᴏɴᴇ : <code>{TIMEZONE}</code>\n\n🉐 Vᴇʀsɪᴏɴ : <code>v{__version__} (Layer {layer})</code></b>")  # Repo : {__repo__}\n Copyright : {__copyright__}           
-            except Unauthorized:             
+                await self.send_message(
+                    LOG_CHANNEL,
+                    text=(
+                        f"<b>{me.mention} is Restarted !!\n\n"
+                        f"📅 Date : <code>{date}</code>\n"
+                        f"⏰ Time : <code>{time}</code>\n"
+                        f"🌐 Timezone : <code>{TIMEZONE}</code>\n\n"
+                        f"🉐 Version : <code>v{__version__} (Layer {layer})</code></b>"
+                    )
+                )
+            except Unauthorized:
                 LOGGER.warning("Bot isn't able to send message to LOG_CHANNEL")
             except BadRequest as e:
                 LOGGER.error(e)
-                                         
 
     async def stop(self, *args):
         await super().stop()
         me = await self.get_me()
-        logging.info(f"{me.first_name} is_...  ♻️Restarting...")
+        logging.info(f"{me.first_name} ♻️Restarting...")
 
-    async def iter_messages(self, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:                       
+    async def iter_messages(self, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:
         current = offset
         while True:
             new_diff = min(200, limit - current)
             if new_diff <= 0:
                 return
-            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
+            messages = await self.get_messages(chat_id, list(range(current, current + new_diff + 1)))
             for message in messages:
                 yield message
                 current += 1
 
 
-        
 app = Bot()
-app.run()
 
-
-
-
-
+# Safe run with FloodWait handling
+while True:
+    try:
+        app.run()
+    except FloodWait as e:
+        logging.warning(f"Global FloodWait! Sleeping {e.value} seconds.")
+        asyncio.sleep(e.value)
+        continue
